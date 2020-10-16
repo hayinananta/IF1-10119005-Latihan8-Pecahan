d576be91174b53be3c9ef771ff1a71eb3d688a49
hayinanjay <72958200+hayinanjay@users.noreply.github.com>
2020-10-16 11:00:12 +0700

    Add files via upload

---
+++ src/pbo1/pkg10119005/latihan8/PBO110119005Latihan8.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10119005.latihan8;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Pecahan
+ */
+public class PBO110119005Latihan8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("float : " + f);
+        System.out.println("double : " + d);
+    }
+    
+}
